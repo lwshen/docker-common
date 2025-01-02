1960b55d85a57acaaca7a79628decf07da1c10ba
Ryo Shen <shenlw66@gmail.com>
2025-01-02 16:18:18 +0800

    fix: correct cron job execution path in run.sh

--- postgres-backup-s3/run.sh
+++ postgres-backup-s3/run.sh
@@ -9,6 +9,5 @@ fi
 if [ "${SCHEDULE}" = "**None**" ]; then
   sh backup.sh
 else
-  echo -e "SHELL=/bin/sh\n${SCHEDULE} /bin/sh /backup.sh" > /etc/crontabs/root
-  exec go-crond /etc/crontabs/root
+  exec go-cron "$SCHEDULE" /bin/sh backup.sh
 fi
