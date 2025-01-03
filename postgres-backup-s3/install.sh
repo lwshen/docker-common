b9badea309530915962463135d693d189485f2ca
Ryo Shen <shenlw66@gmail.com>
2025-01-03 12:01:18 +0800

    fix(install.sh): add missing ca-certificates package

--- postgres-backup-s3/install.sh
+++ postgres-backup-s3/install.sh
@@ -4,7 +4,7 @@
 set -eux
 
 apt-get update -qq
-apt-get install -qq --no-install-recommends openssl postgresql-client unzip curl
+apt-get install -qq --no-install-recommends ca-certificates openssl postgresql-client unzip curl
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
