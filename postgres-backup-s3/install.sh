94cfc684ecc568b0de5a20e1898799fabacb492d
Ryo Shen <shenlw66@gmail.com>
2025-01-03 13:07:43 +0800

    feat(install): add PostgreSQL apt repository setup

--- postgres-backup-s3/install.sh
+++ postgres-backup-s3/install.sh
@@ -4,7 +4,12 @@
 set -eux
 
 apt-get update -qq
-apt-get install -qq --no-install-recommends ca-certificates openssl postgresql-client unzip curl
+apt-get install -qq --no-install-recommends ca-certificates openssl unzip curl postgresql-common
+
+# Add official PostgreSQL apt repository to not depend on Debian's version.
+#   https://www.postgresql.org/download/linux/debian/
+/usr/share/postgresql-common/pgdg/apt.postgresql.org.sh -y
+apt-get install -qq --no-install-recommends postgresql-client-17
 
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
