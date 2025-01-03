53e5f941d12ddae393e41914224ec5c747fca08e
Ryo Shen <shenlw66@gmail.com>
2025-01-03 11:53:46 +0800

    build: switch to snap for aws-cli installation

--- postgres-backup-s3/install.sh
+++ postgres-backup-s3/install.sh
@@ -4,7 +4,8 @@
 set -eux
 
 apt-get update -qq
-apt-get install -qq --no-install-recommends openssl aws-cli postgresql-client
+snap install aws-cli --classic
+apt-get install -qq --no-install-recommends openssl postgresql-client
 
 # cleanup
 rm -rf /var/lib/apt/lists/*
